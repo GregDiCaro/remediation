a4535296866197d92b5d800087c2776313c82937
Di Caro Gregory <dicaro.gregory@gmail.com>
2020-07-01 12:15:14 +0200

    exo api meteo

---
+++ meteo/app.js
@@ -0,0 +1,34 @@
+const APIKEY ="c0eeac2ce2be28fb5b3a1d5bd61f4ca0";
+
+let apiCall = function(city){
+
+    let url =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}&units=metric&lang=fr`;
+
+// api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=c0eeac2ce2be28fb5b3a1d5bd61f4ca0
+
+fetch(url).then((response) =>
+    
+    response.json().then((data) => {
+
+        console.log(data);
+        document.querySelector("#city").innerHTML= data.name;
+        document.querySelector("#temp").innerHTML= data.main.temp + "°";
+        document.querySelector("#humidity").innerHTML= `<i class="fas fa-tint"></i>`+ data.main.humidity + "%";
+        document.querySelector("#wind").innerHTML= `<i class='fas fa-wind'></i>`+ data.wind.speed + "km/h";
+    })
+    
+        
+    ).catch(err => console.log('Erreur : '+ err));
+    
+
+};
+
+    document.querySelector('form').addEventListener('submit', function(e){
+
+            e.preventDefault();
+            let ville = document.querySelector("#inputcity").value;
+
+            apiCall(ville);
+    });
+
+    apiCall("Liege");
